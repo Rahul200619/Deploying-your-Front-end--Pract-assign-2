7dca4800d4b4a7d134f593292c6a1a8bdf87a120
Rahul2006019 <vigneshrahulr@gmail.com>
2025-03-04 17:15:57 +0530

    updated

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hi, I'm [Your Name]!</h1>
+      <p>📞 Contact: +91 XXXXX XXXXX</p>
+      <p>📧 Email: yourname@email.com</p>
+    </div>
+  );
+}
+
+export default App;
